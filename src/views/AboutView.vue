3f69d5063499f9711017470042e24f23f4eba1ff
Neni sari putri juana <56350878+Nenisariputrijuana@users.noreply.github.com>
2022-12-23 11:29:12 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
        <h1>Biodata</h1>
-    <p> Nama : Fatma Hayati</p>
-    <p> Nim : 11950125061</p>
+    <p> Nama : Neni Sari Putri Juana</p>
+    <p> Nim : 11950123432</p>
   </div>
   </div>
 </template>
