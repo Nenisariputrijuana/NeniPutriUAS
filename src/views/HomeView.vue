f9a2a905b1f655a39fbe6fc6787b7fdee7c3ed07
Neni sari putri juana <56350878+Nenisariputrijuana@users.noreply.github.com>
2022-12-23 11:29:55 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <h1>Belajar form dan Vue Event</h1>
+
+<div class="container">
+ 
+  <p>Nama</p>
+  <input v-model="nama" type="text" class="form-text">
+  <p>Nim</p>
+  <input v-model="nim" type="text" class="form-text">
+  <div class="mt-2">
+  <button  type="submit" class="btn btn-info" @click.prevent="simpanData">Simpan</button>
+</div>
+</div>
+
+<hr>
+<table class="table">
+<thead>
+  <tr>
+    <th>Nama</th>
+    <th>Nim</th>
+  </tr>
+</thead>
+<tbody>
+  <tr v-for="item in dataMahasiswa">
+    <td>{{item.nama}}</td>
+    <td>{{item.nim}}</td>
+  </tr>
+</tbody>
+</table>
 </template>
 
 <script>
