e98b8080aca60e2a5c08ed3f43449fe4b419fea7
Neni sari putri juana <56350878+Nenisariputrijuana@users.noreply.github.com>
2022-12-23 11:28:48 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+       <h1>Biodata</h1>
+    <p> Nama : Fatma Hayati</p>
+    <p> Nim : 11950125061</p>
+  </div>
   </div>
 </template>
